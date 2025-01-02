f63d8ecf1fa328c014206c37f73cf0dd6ac7b82f
Dharshu <dharshiniduraisamy2801@gmail.com>
2025-01-02 21:40:45 +0530

    Service page

--- src/app/service/service.component.ts
+++ src/app/service/service.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-service',
   standalone: true,
   imports: [],
   templateUrl: './service.component.html',
-  styleUrl: './service.component.css'
+  styleUrl: './service.component.css',
 })
 export class ServiceComponent {
+  constructor(private router: Router) {}
 
+  about() {
+    this.router.navigate(['about']);
+  }
+  services() {
+    this.router.navigate(['service']);
+  }
+  projects() {
+    this.router.navigate(['projects']);
+  }
+  contact() {
+    this.router.navigate(['contact']);
+  }
 }
