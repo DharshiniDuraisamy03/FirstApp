ed985f85ae08607d0e6ed7936a24da6fc276a7e0
Dharshu <dharshiniduraisamy2801@gmail.com>
2025-01-04 12:47:46 +0530

    Contact

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
   imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
 })
 export class ContactComponent {
-
+  constructor(private router: Router) {}
+  home() {
+    this.router.navigate(['home']);
+  }
+  about() {
+    this.router.navigate(['about']);
+  }
+  services() {
+    this.router.navigate(['service']);
+  }
+  projects() {
+    this.router.navigate(['projects']);
+  }
+  contact() {
+    this.router.navigate(['contact']);
+  }
 }
